d33c644f5ccf26d8e52b9f6574f3369a454dec00
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2022-03-03 22:36:02 -0500

    Memorize login status
    Use effect and local storage to memorize login status

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const uLoggedInInfo = localStorage.getItem("uIsLoggedIn");
+    if (uLoggedInInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("uIsLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.setItem("uIsLoggedIn", "0");
     setIsLoggedIn(false);
   };
 
